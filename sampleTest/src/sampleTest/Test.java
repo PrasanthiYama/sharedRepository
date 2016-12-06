304cc2a172b19a9bfc7f09d29b2ea633aba9c545
PrasanthiYama <yama.prasanthi@gmail.com>
2016-12-06 16:56:52 +0530

    Update Test.java

--- sampleTest/src/sampleTest/Test.java
+++ sampleTest/src/sampleTest/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("git test1"); 
-		System.out.println("git test1"); 
+		System.out.println("git test12"); 
 	}
 
 }
